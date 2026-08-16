9b2245c8668a650300c7e652d55f12ce493e5237
Tanvi Milind Gurav <72682763+Tanvi3103@users.noreply.github.com>
2026-08-16 15:07:50 +0530

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int n = nums.Length;
+        k %= n;
+
+        Reverse(nums, 0, n - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, n - 1);
+    }
+
+    private void Reverse(int[] nums, int l, int r)
+    {
+        while (l < r)
+        {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
